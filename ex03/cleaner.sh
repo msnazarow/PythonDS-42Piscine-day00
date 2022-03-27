06adefb039fb6a0d4765b92d8a98e41e70ae4f19
Михаил Назаров <msnazarow@gmail.com>
2022-03-27 14:41:46 +0300

    fix: remove unused

--- ex03/cleaner.sh
+++ ex03/cleaner.sh
@@ -8,7 +8,5 @@ fi
 GRADES='Junior|Middle|Senior|junior|middle|senior'
 FIRST_TWO_COLUMN='"[^"]*","[^"]*",'
 ADRESS=$FIRST_TWO_COLUMN'"[^"]*'$GRADES'[^"]*",'
-FIRST
 
-sed -r '/'$ADRESS'/!s~('$FIRST_TWO_COLUMN')"[^"]*"~\1"-"~g; /'$ADRESS'/s~('$FIRST_TWO_COLUMN')"([^"]*('$GRADES')[^"]*)+"~\1"\2"~g' ${CSV_FILE}
-#sed -r '/[^,]*,[^,]*,.*Senior|Middle|Junior.*/!s' $CSV_FILE
+sed -r '/'$ADRESS'/!s~('$FIRST_TWO_COLUMN')"[^"]*"~\1"-"~g; /'$ADRESS'/s~('$FIRST_TWO_COLUMN')"[^"]*('$GRADES')[^"]*"~\1"\2"~g' ${CSV_FILE}
