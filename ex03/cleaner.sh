e827e47818807de91761d522c7afea91e5e2e8af
Michael <msnazarow@gmail.com>
2022-03-28 03:14:04 +0300

    feat: ex03

--- ex03/cleaner.sh
+++ ex03/cleaner.sh
@@ -6,31 +6,25 @@ if [ $# -eq 0 ]
 fi
 
 GRADES='(Junior|Middle|Senior|junior|middle|senior)'
-FIRST_TWO_COLUMN='("[^"]*",){2}'
+FIRST_TWO_COLUMN='"[0-9]*","[^"]*",'
 ADRESS=$FIRST_TWO_COLUMN'"[^"]*'$GRADES'[^"]*",'
 
-#sed -r '/'$ADRESS'/!s~('$FIRST_TWO_COLUMN')"[^"]*"~\1"-"~g; /'$ADRESS'/s~('$FIRST_TWO_COLUMN')"([^"]*'$GRADES'[^"]*)+"~\1"SOSAT"~2' ${CSV_FILE}
-
-#GRADES='Junior|Middle|Senior|junior|middle|senior'
-#FIRST_TWO_COLUMN='("[^"]*",){2}'
-#ADDRESS=$FIRST_TWO_COLUMN'"[^"]*'$GRADES'[^"]*",'
-#MINUS_RULE='/'$ADDRESS'/!s~('$FIRST_TWO_COLUMN')"[^"]*"~\1"-"~;'
-#sed -r $MINUS_RULE'/'$ADDRESS'/{s~('$GRADES')~ツ\1ツ~ig; s~".*ツ~~2}' ${CSV_FILE}
-
-#echo "LOL KEK ;bac bac bac bac " | sed -rn '/.*a.*/ {s/(.*;)[^a]*a[^a]*/\1d /; l}'
-#echo "LOL KEK ;qaq qbq qcq qaq; eat coocies" | sed -r ':label; s/;[^;]*(a|b|c)([^;]*);/;\1\2;/; h;l; tlabel;'
-echo -n "LOL KEK ;qqqqqqqMashabbbbbbbbbbbSashaeeeeeeeeeeeeeeeeeMashaffffff" | sed -rn 'h;s/([^;]*;).*/\1/;s/\x00//;p;s/\n//; x; s/([^;]*;)(.*)/\2/;
-h;:label;
-s/(.*)(Masha|Sasha)(.*)/\1/;x;
-s/(.*)(Masha|Sasha)(.*)/\2/;p;g;
-Tlabel2;
-tlabel;
-:label2;
-2s/(.*)/\1\n/'
-#echo -n "LOL KEK ;qaq qaq qaq qaq " | sed -rn 'H;s/([^;]*;).*/\1/; s/\x0//; p; x; s/([^;]*;)(.*)/\2/; s/[^a]*a[^a]*/a /g; s/(.*)/\1\n/;p'
-#echo
-#echo -e "\x00"
-
-#echo "qqqqqqqMashabbbbbbbbbbbSashaeeeeeeeeeeeeeeeeeMashaffffff" | sed  -rn ':label;
-#h;s/(.*)(Masha|Sasha)/\1/; x; s/(.*)(Masha|Sasha)(.*)/\2/;p;x;
-#tlabel;'
+sed -r '/'$ADRESS'/!s~('$FIRST_TWO_COLUMN')"[^"]*"~\1"-"~;
+/'$ADRESS'/{
+h;
+s/('$FIRST_TWO_COLUMN').*/\1/;
+H;
+g;
+s/([^\n]*)\n.*/\1/;
+s/'$ADRESS'//;
+H;
+g;
+s/([^\n]*)\n.*/\1/;
+s/('$GRADES')|(.)/\1/g;
+s/'$GRADES'/\1\//g;
+s/'$GRADES'\/$/\1/;
+H;
+x;
+s/[^\n]+\n([^\n]+)\n([^\n]+)\n([^\n]+)/\1\3,\2/;
+}
+' ${CSV_FILE} > hh_positions.csv
